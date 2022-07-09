324ba1c0f5f86b7d7aaa6b53841599814dc2d60d
Alejandro_Ferrera <aferrerah1421@gmail.com>
2022-07-09 10:41:37 -0400

    added discount funcionality and display for categories

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const cartItems = document.querySelector('.cart-items');
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
+const categoriesDOM = document.querySelector('.categories');
 
 let cart = [];
 let buttonsDOM = [];
@@ -23,10 +24,39 @@ class Products {
     }
 }
 
+class Categories {
+    async getCategories() {
+        try {
+            let result = await fetch("http://127.0.0.1:8000/get_categories");
+            let data = await result.json();
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
 class UI {
+    displayCategories(categories) {
+        let result = "";
+        categories.forEach(category => {
+            result += `
+            <button class="category-btn">${category.name}</button>
+            `
+        })
+        categoriesDOM.innerHTML = result;
+    }
+
     displayProducts(products) {
         let result = "";
+
+
+
+
         products.forEach(product => {
+
+            let haveDiscount = product.discount != 0;
+
             result += `
             <article class="product">
                 <div class="img-container">
@@ -37,9 +67,21 @@ class UI {
                         Agregar
                     </button>
                 </div>
-                <h3>${product.name}</h3>
-                <h4>${product.price.toLocaleString('es-ES', { style: 'currency', currency: 'CLP' })}</h4>
-            </article>`;
+                <h3>${product.name}</h3>`;
+
+            if (haveDiscount) {
+
+                let newPrice = product.price - (product.price * product.discount / 100);
+
+                result += `<h3 style="text-decoration:line-through; margin-bottom:0;">${product.price}</h3>
+                           <h4 style="margin-top: 1px;">${newPrice.toLocaleString('es-ES', { style: 'currency', currency: 'CLP' })}
+                           </h4> 
+                           </article>`;
+
+            } else {
+                result += `<h4>${product.price.toLocaleString('es-ES', { style: 'currency', currency: 'CLP' })}</h4> 
+                           </article>`;
+            }
 
         });
         productsDOM.innerHTML = result;
@@ -59,7 +101,7 @@ class UI {
                 event.target.innerText = "En carro";
                 event.target.style.color = 'gray';
                 event.target.disabled = true;
-                
+
                 let cartItem = Storage.getProduct(id);
                 console.log(cartItem);
             })
@@ -82,6 +124,12 @@ class Storage {
 document.addEventListener("DOMContentLoaded", () => {
     const ui = new UI();
     const products = new Products();
+    const categories = new Categories();
+
+    categories.getCategories().then(category => {
+        ui.displayCategories(category);
+    })
+
     products.getProducts().then(data => {
         ui.displayProducts(data)
         Storage.saveProducts(data)
