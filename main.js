46861e06745237c7df1fc74169e95d9160c91551
Alejandro_Ferrera <aferrerah1421@gmail.com>
2022-07-09 04:52:49 -0400

    get all products locally

--- main.js
+++ main.js
@@ -1,49 +1,91 @@
-let productContainer = document.querySelector('#products-container')
-let raiz = document.getElementById('main')
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
 
-const card = (product_name, product_price, url_image) => {
-    return (
-        `<div class="col">
-            <div class="card" style="width: 18rem;">
-            <img src=${url_image} class="card-img-top" alt="imagen de ${product_name}">
-            
-            <div class="card-body">
-                <h4 class="card-title">${product_name}</h4>
-                <h6 class="card-text text-success">${product_price}</h6>
-            </div>
+let cart = [];
+let buttonsDOM = [];
 
-        </div>
-    </div>`);
-}
-
-let getProducts = async (category = "") => {
-    let response = await fetch('http://127.0.0.1:8000/get_products')
-    let products = await response.json()
-    console.log(products)
-}
-
-let getProductsByCategory = async () => {
-    let response = await fetch('http://127.0.0.1:8000/get_categories')
-    let categories = await response.json()
-
-    for (let index = 0; index < categories.length; index++) {
-        const category = categories[index].name;
-
-        let categoryProducts = await fetch(`http://127.0.0.1:8000/get_products?category=${category}`)
-        let products = await categoryProducts.json()
-
-        for (let index = 0; index < products.length; index++) {
-            const product = products[index];
-            let box = card(product.name, product.price, product.url_image);
-            productContainer.innerHTML += box;
+class Products {
+    async getProducts() {
+        try {
+            let result = await fetch('products.json');
+            let data = await result.json();
+            return data;
+        } catch (error) {
+            console.log(error);
         }
-
     }
 }
 
+class UI {
+    displayProducts(products) {
+        let result = "";
+        products.forEach(product => {
+            result += `
+            <article class="product">
+                <div class="img-container">
+                    <img src=${product.url_image == null || product.url_image == '' ? "missing.png" : product.url_image} alt="imagen de ${product.name}"
+                        class="product-img">
+                    <button class="bag-btn" data-id=${product.id}>
+                        <i class="fas fa-shopping-cart"></i>
+                        Agregar
+                    </button>
+                </div>
+                <h3>${product.name}</h3>
+                <h4>${product.price.toLocaleString('es-ES', { style: 'currency', currency: 'CLP' })}</h4>
+            </article>`;
+
+        });
+        productsDOM.innerHTML = result;
+    }
+    getBagButtons() {
+        const buttons = [...document.querySelectorAll(".bag-btn")];
+        buttonsDOM = buttons;
+        buttons.forEach(button => {
+            let id = button.dataset.id;
+            let inCart = cart.find(item => item.id === id)
+
+            if (inCart) {
+                button.innerText = "En carro";
+                button.disabled = true;
+            }
+            button.addEventListener('click', (event) => {
+                event.target.innerText = "En carro";
+                event.target.style.color = 'gray';
+                event.target.disabled = true;
+                
+                let cartItem = Storage.getProduct(id);
+                console.log(cartItem);
+            })
+        })
+    };
+}
 
 
-getProducts()
-getProductsByCategory()
 
+class Storage {
+    static saveProducts(products) {
+        localStorage.setItem("products", JSON.stringify(products))
+    }
+    static getProduct(id) {
+        let products = JSON.parse(localStorage.getItem('products'));
+        return products.find(product => product.id === id)
+    }
+}
 
+document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UI();
+    const products = new Products();
+    products.getProducts().then(data => {
+        ui.displayProducts(data)
+        Storage.saveProducts(data)
+    }).then(() => {
+        ui.getBagButtons()
+    });
+});
